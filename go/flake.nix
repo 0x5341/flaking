0583b20a9f221e46eb75913a793ff4ff362ee076
0x5341 <107183580+0x5341@users.noreply.github.com>
2025-11-21 21:37:14 +0900

    fix go simple

--- go/flake.nix
+++ go/flake.nix
@@ -6,34 +6,47 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, utils, ... }:
-    utils.lib.eachSystem utils.lib.allSystems (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      ci-pkgs = with pkgs; [ go golangci-lint gotests go-tools ];
-      godoctor = pkgs.buildGoModule (final: {
-        name = "godoctor";
-        src = pkgs.fetchFromGitHub {
-          owner = "godoctor";
-          repo = "godoctor";
-          rev = "v0.8";
-          hash = "sha256-RUE8lPWHLfh2KvUwlGSOdzNuJJigWctFwIwKENf3CTA=";
+  outputs =
+    { nixpkgs, utils, ... }:
+    utils.lib.eachSystem utils.lib.allSystems (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        godoctor = pkgs.buildGoModule (final: {
+          name = "godoctor";
+          src = pkgs.fetchFromGitHub {
+            owner = "godoctor";
+            repo = "godoctor";
+            rev = "v0.8";
+            hash = "sha256-RUE8lPWHLfh2KvUwlGSOdzNuJJigWctFwIwKENf3CTA=";
+          };
+          vendorHash = null;
+          doCheck = false;
+        });
+      in
+      {
+        devShells = rec {
+          dev = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                go
+                golangci-lint
+                gotests
+                go-tools
+                gopls
+                gotools
+                reftools
+                gomodifytags
+                gopkgs
+                impl
+                godef
+
+              ]
+              ++ [ godoctor ];
+          };
+          default = dev;
         };
-        vendorHash = null;
-        doCheck = false;
-      });
-      dev-pkgs = (
-        with pkgs; [ gopls gotools reftools gomodifytags gopkgs impl godef ]
-      ) ++ [ godoctor ];
-    in {
-      devShells = rec {
-        dev = pkgs.mkShell {
-          packages = ci-pkgs ++ dev-pkgs;
-        };
-        ci = pkgs.mkShell {
-          packages = ci-pkgs;
-        };
-        default = dev;
-      };
-    });
+      }
+    );
 }
